46526683000053ba4df98e4fdd1fbab2a8f65ac6
abhi1kumar <abhinav3663@gmail.com>
2020-02-11 12:51:58 -0700

    q1 refactored

--- assignment_1/function/q1.py
+++ assignment_1/function/q1.py
@@ -4,10 +4,7 @@ import os, sys
 sys.path.append(os.getcwd())
 
 import numpy as np
-from scipy.spatial.distance import cdist as dist
 import pandas as pd
-from sklearn.cluster import KMeans
-
 import matplotlib.pyplot as plt
 
 from util import *
@@ -18,51 +15,11 @@ fs    = 15
 lw    = 2
 matplotlib.rcParams.update({'font.size': fs})
 
-def kcenter(data, num_clusters= 2, metric= "euclidean"):
-    """
-        Implements a k-center algorithm
-    """
-    N = data.shape[0]
-
-    # Initialize cluster centers to -1
-    center_indices = -np.ones((num_clusters,), dtype=np.int32)   
-
-    # Compute the distance between every pair of points    
-    dist_mat = dist(data, data, metric= metric) # N x N
-
-    # Compute centers
-    for i in range(num_clusters):
-        if i == 0:
-            # Choose an id arbitrarily from N
-            new_center_id = np.random.randint(N)
-        else:
-            # Choose maximum of the minimum distance of the previous center indices
-            new_center_id = np.argmax(np.min(dist_mat[center_indices[0:i]], axis= 0))
-
-        center_indices[i] = new_center_id
-
-    # Get the labels of the points
-    cluster_labels = np.argmin(dist_mat[center_indices], axis= 0)   
-
-    # Get the cost of clustering
-    cost = np.max(np.min(dist_mat[center_indices], axis= 0))
-
-    return data[center_indices], cluster_labels, cost
-
-def kmeans(data, num_clusters):
-    """
-        Implements k-means algorithm
-        Reference https://towardsdatascience.com/k-means-clustering-with-scikit-learn-6b47a369a83c
-    """
-    km = KMeans(n_clusters= num_clusters, init='random',  n_init= 1, max_iter= 300, 
-        tol=1e-04, random_state=0)
-    cluster_labels_km = km.fit_predict(data)
-
-    cost = km.inertia_/data.shape[0]
-
-    return km.cluster_centers_, cluster_labels_km, cost
-
 def run_k_center_k_means(data, num_clusters= 3):
+    """
+        Runs k-center and k-means on the same data and plots the outputs side by
+        side
+    """
     cluster_centers_kc, cluster_labels_kc, _ = kcenter(data, num_clusters)
     cluster_centers_km, cluster_labels_km, _ = kmeans (data, num_clusters)
 
@@ -80,7 +37,7 @@ def run_k_center_k_means(data, num_clusters= 3):
     plt.close()
 
 #===============================================================================
-# Main function
+# Execution starts here
 #===============================================================================
 # Read the data from csv
 # The zeroth column should be used as index
